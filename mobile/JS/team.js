905a631b1f7602d1cce72eff3b6d8051361adfbc
harrisonmiller <harrison918@aol.com>
2019-04-01 14:18:59 -0400

    Adjust window size handling in javascript

--- mobile/JS/team.js
+++ mobile/JS/team.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+  var width = $(window).width();
+  if(!(width<992)){
+    window.location.replace("/HTML/team.html");
+  }
+
   $(window).resize(function() {
     width = $(window).width();
     if(!(width<992)){
