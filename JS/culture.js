3d6d4a17439879c9867311f29517bb20116bbb2c
harrisonmiller <harrison918@aol.com>
2019-04-08 11:49:30 -0400

    Fixed some file directories in the javascript

--- JS/culture.js
+++ JS/culture.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
 
     var width = $(window).width();
     if(!(width>=992)){
-      window.location.replace("/mobile/HTML/culture.html");
+      window.location.replace("../mobile/HTML/culture.html");
     }
 
     $(window).resize(function() {
       width = $(window).width();
       if(!(width>=992)){
-        window.location.replace("/mobile/HTML/culture.html");
+        window.location.replace("../mobile/HTML/culture.html");
       }
     });
 
--- JS/forwardMotion.js
+++ JS/forwardMotion.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
 
     var width = $(window).width();
     if(!(width>=992)){
-      window.location.replace("/mobile/HTML/forwardMotion.html");
+      window.location.replace("../mobile/HTML/forwardMotion.html");
     }
 
     $(window).resize(function() {
       width = $(window).width();
       if(!(width>=992)){
-        window.location.replace("/mobile/HTML/forwardMotion.html");
+        window.location.replace("../mobile/HTML/forwardMotion.html");
       }
     });
 
