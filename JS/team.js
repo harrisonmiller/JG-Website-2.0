53a6347e0e1a7a2652bce132f361690452163d7e
harrisonmiller <harrison918@aol.com>
2019-04-15 14:09:23 -0400

    Fixed paths from mobile specific section

--- JS/team.js
+++ JS/team.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
 
   var width = $(window).width();
   if(!(width>=992)){
-    window.location.replace("/mobile/HTML/team.html");
+    window.location.replace("https://harrisonmiller.github.io/JG-Website-2.0/mobile/HTML/team.html");
   }
 
   $(window).resize(function() {
     width = $(window).width();
     if(!(width>=992)){
-      window.location.replace("/mobile/HTML/team.html");
+      window.location.replace("https://harrisonmiller.github.io/JG-Website-2.0/mobile/HTML/team.html");
     }
   });
 
--- mobile/JS/forwardMotion.js
+++ mobile/JS/forwardMotion.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
 
     var width = $(window).width();
     if(!(width<992)){
-      window.location.replace("../HTML/forwardMotion.html");
+      window.location.replace("https://harrisonmiller.github.io/JG-Website-2.0/HTML/forwardMotion.html");
     }
 
     $(window).resize(function() {
       width = $(window).width();
       if(!(width<992)){
-        window.location.replace("../HTML/forwardMotion.html");
+        window.location.replace("https://harrisonmiller.github.io/JG-Website-2.0/HTML/forwardMotion.html");
       }
     });
 
